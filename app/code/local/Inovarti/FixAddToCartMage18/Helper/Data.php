eee040e22b90c28f3984202e7dd685376eaccd2a
Deivison Arthur Lemos Serpa <deivison.arthur@gmail.com>
2014-02-02 19:57:47 -0200

    Primeiro Envio
    The module was created because it is the new "form_key" parameter added
    to POST requests in Magento 1.8 to Prevent XSS attacks. It causes
    troubles with Varnish because the product page is cached with the first
    session of the form_key visiting the website, and the others visitors
    can not csrf check because it does not match with Their form_key
    session.
    
    For "add to cart" event, I've added to the fixcart where I form_key
    replace the parameter with the session form_key to pass the csrf check
    and let customers add products to cart.
    It disables the csrf protection for the "add to cart" action but it's
    just like Magento <1.8

---
+++ app/code/local/Inovarti/FixAddToCartMage18/Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Inovarti FixAddToCartMage18 Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Open Software License version 3.0
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * deivison.arthur@gmail.com so we can send you a copy immediately.
+ *
+ */
+class Inovarti_FixAddToCartMage18_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
